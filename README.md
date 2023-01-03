adaa55d6d1f6534a6a3120849264b980b080acc2
Henry Troncoso <henry.troncoso@cencosud.cl>
2023-01-03 15:59:23 -0300

    :boom: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # cognito-authentication-client
 frontend client to use with cognito-authentication
+
+## Install
+```
+npm install cognito-authentication-client
+```
+
+## Requirements
+- Use with Vite
+
+Environment variables
+```
+VITE_APP_NAME=<example_app>
+VITE_LOGIN_URL=<example http://localhost:3000/login>
+```
+
+## Usage
+```
+import CognitoAuthentication from 'cognito-authentication-client'
+
+const cognito = new CognitoAuthentication()
+
+cognito.isAuthenticated() // It should show if exists access_token stored
+cognito.login() // It redirect and manipulate the login process
---
+++ src/index.js
@@ -0,0 +1,61 @@
+const {
+  VITE_APP_NAME: appName,
+  VITE_LOGIN_URL: loginURL
+} = import.meta.env
+
+class CognitoAuthentication {
+  #appName
+  #loginURL
+  #storage
+
+  constructor () {
+    this.#appName = appName
+    this.#loginURL = loginURL
+    this.#storage = sessionStorage
+  }
+
+  #setToken ({ access_token: token }) {
+    this.#storage.setItem(`${this.#appName}_TOKEN`, token)
+  }
+
+  #fetchData (url) {
+    const { origin, pathname } = window.location
+    const headers = {
+      originalUrl: `${origin}${pathname}`
+    }
+    return fetch(url, { headers })
+      .then((response) => response.json())
+  }
+
+  #getToken (url) {
+    return this.#fetchData(url)
+      .then((data) => data)
+      .catch((error) => console.log(error))
+  }
+
+  #redirectLogin (url) {
+    this.#fetchData(url)
+      .then((data) => window.location.href = data.loginURL)
+      .catch((error) => console.error(error))
+  }
+
+  isAuthenticated () {
+    const token = this.#storage.getItem(`${this.#appName}_TOKEN`)
+    return !!token
+  }
+
+  login () {
+    const params = new URLSearchParams(window.location.search)
+    const code = params.get('code')
+    if (code) {
+      const url = `${this.#loginURL}?code=${code}`
+      return this.#getToken(`${this.#loginURL}?code=${code}`)
+        .then((sessionData) => { 
+          if (sessionData) this.#setToken(sessionData)
+        })
+    }
+    return this.#redirectLogin(this.#loginURL)
+  }
+}
+
+export default CognitoAuthentication
